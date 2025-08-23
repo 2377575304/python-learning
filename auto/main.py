821637afbcdfed199e91a992232ceb676bda1a5b
xiaoyuxuan <2377575304@qq.com>
2025-08-23 19:47:31 +0800

    docs: 添加项目README文件
    初始化项目文档，包含项目标题和基本介绍

--- auto/main.py
+++ auto/main.py
@@ -1,8 +1,32 @@
+#auther:xiao
+#date:2023-08-10
+#version:1.0
+#description:自动点击s键(自动刷抖音)
 import pyautogui
 import time
+import keyboard 
+import threading
+
+#running = True
+#stop_event = threading.Event()
 
 
 
+def main():
+    # get_position()  ss
+    print("press esc to exits")
+    while True:
+        pyautogui.press("s") 
+        time.sleep(5)
+        if keyboard.is_pressed('esc'):  
+             break
 
-if name == '__main__':
+def get_position():
+    while True:
+        x, y = pyautogui.position()
+        print(x, y)
+        time.sleep(0.5)    
+
+
+if __name__ == '__main__':
     main()
