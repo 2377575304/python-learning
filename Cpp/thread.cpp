c1ece4cc84c38ed27a27e913d6d28dcbfcd4043e
xiaoyuxuan <2377575304@qq.com>
2025-08-28 01:58:41 +0800

    feat(thread): 添加互斥锁保护并优化线程示例
    - 引入mutex保护printhello函数中的共享资源
    - 缩短线程休眠时间以加快测试速度
    - 添加新线程t4展示指针参数传递
    - 增加引用和指针的打印输出

--- Cpp/thread.cpp
+++ Cpp/thread.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+
+std::mutex mtx;
 
 // 输出消息的函数
 void printMessage1(const std::string& message) {
     for (int i = 0; i < 5; ++i) {
         std::cout << message << std::endl;
         // 模拟耗时操作
-        std::this_thread::sleep_for(std::chrono::seconds(10));
+        std::this_thread::sleep_for(std::chrono::seconds(3));
     }
+}
+
+void printhello(int *a)
+{   
+    std::lock_guard<std::mutex> lock(mtx);//构造时自动枷锁
+    
+    for(int i =0;i<*a;i++)
+    {
+        std::cout<<"hello world"<<a<<std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    //自动解锁
 }
 
 void printMessage2(const std::string& message) {
     for (int i = 0; i < 5; ++i) {
         std::cout << message << std::endl;
         // 模拟耗时操作
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 }
 
 int main() {
+
+    int a = 2;
+    int* ptr = &a;
+    
     // 创建两个线程，分别执行不同的任务
     std::thread t1(printMessage1, "线程 1 正在运行");
 
     //执行lambda表达式
     std::thread t3([/*cipture 外部捕获的值*/](/*传递的参数*/){
-        for(int i =0;i<99;i++){std::cout<<i<<"\n";
+        for(int i =0;i<10;i++){std::cout<<i<<"\n";
         std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     });
     std::thread t2(printMessage2, "线程 2 正在运行");
+    std::thread t4(printhello, ptr);//引用传递
+    std::cout<<std::ref(a)<<"\n";   //输出引用地址
+    std::cout<<&a<<"\n";            //输出变量地址
+    std::cout<<ptr<<"\n";       //输出引用指向的变量值
+    std::cout<<a<<"\n";             //输出变量值
 
     // 等待线程 t1 执行完毕
     if (t1.joinable()) {
@@ -37,6 +61,13 @@ int main() {
     // 等待线程 t2 执行完毕
     if (t2.joinable()) {
         t2.join();
+    }
+    // 等待线程 t3 执行完毕
+    if (t3.joinable()) {
+        t3.join();
+    }
+     if (t4.joinable()) {
+        t4.join();
     }
 
     std::cout << "所有线程执行完毕，主线程结束。" << std::endl;
